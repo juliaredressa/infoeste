0b44179da9063af3670b7185fcfbb12e04120690
Julia Redressa <julialimaredressa@gmail.com>
2024-10-31 19:43:58 -0300

    Ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'unoeste-inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'unoeste-inicio',
+        loadComponent: () => import('./pages/inicio/inicio.component').then(c => c.InicioComponent)
+    }
+];
