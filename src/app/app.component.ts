9fe18ea534d5a72903e99cd15281e67a053157c8
Julia Redressa <julialimaredressa@gmail.com>
2024-10-31 14:40:05 -0300

    Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotaoComponent } from "./botao/botao.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'curso-infoeste';
+  
+  textodobotao = 'Texto que veio do servidor';
+
+  clicouNoBotao(textoAlterado: string) {
+    alert(textoAlterado);
+  }
 }
---
+++ src/app/botao/botao.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-botao',
+  standalone: true,
+  imports: [],
+  templateUrl: './botao.component.html',
+  styleUrl: './botao.component.css'
+})
+
+export class BotaoComponent {
+  //recebi
+  @Input() texto: string = 'Clique aqui';
+  //mandei
+  @Output() redressa = new EventEmitter();
+
+  clicoDoComponent(){
+    this.redressa.emit();
+  }
+
+  //evento enviado
+  // clicoDoComponent(){
+  
+  //   this.redressa.emit('Veio pelo evento');
+  // }
+}
